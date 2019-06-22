c5884499935933ee74a5fff20871576c90895a1b
ramaprakoso1 <rama.wiedprakoso@gmail.com>
2019-06-22 12:14:11 +0700

    simulasi js

--- assets/custom_js/simulation.js
+++ assets/custom_js/simulation.js
@@ -1,10 +1,100 @@
 $(document).ready(function(){
-  $(document).on('change', '#state',function(){
-    console.log($(this).val());
-    // $("#total_unit_group").hide();
-    // $("#harga_jual_group").hide();
-    // $("#harga_variabel_group").hide();
-    // $("#fix_cost_group").hide();
-    // $("#company_target_group").hide();
-  });
+    $(document).on('keyup', '#total_unit', '#harga_jual', function(){
+      var n = $(this).val().replace(/\D/g, '');
+            if (n == '')
+                {
+                    $(this).val('');
+                    return false;
+                }
+                $(this).val(parseInt(n, 10).toLocaleString().replace(/,/g, '.'));
+    });
+
+    $(document).on('keyup', '#harga_jual', function(){
+      var n = $(this).val().replace(/\D/g, '');
+            if (n == '')
+                {
+                    $(this).val('');
+                    return false;
+                }
+                $(this).val(parseInt(n, 10).toLocaleString().replace(/,/g, '.'));
+    });
+
+    $(document).on('keyup', '#harga_variabel', function(){
+      var n = $(this).val().replace(/\D/g, '');
+            if (n == '')
+                {
+                    $(this).val('');
+                    return false;
+                }
+                $(this).val(parseInt(n, 10).toLocaleString().replace(/,/g, '.'));
+    });
+
+    $(document).on('keyup', '#fix_cost', function(){
+      var n = $(this).val().replace(/\D/g, '');
+            if (n == '')
+                {
+                    $(this).val('');
+                    return false;
+                }
+                $(this).val(parseInt(n, 10).toLocaleString().replace(/,/g, '.'));
+    });
+
+    $(document).on('keyup', '#company_target', function(){
+      var n = $(this).val().replace(/\D/g, '');
+            if (n == '')
+                {
+                    $(this).val('');
+                    return false;
+                }
+                $(this).val(parseInt(n, 10).toLocaleString().replace(/,/g, '.'));
+    });
+
+    $(document).on('submit', '#form_cvp', function(e){
+      e.preventDefault();
+      var satu = parseInt($("#total_unit").val().replace(/\./g,''));
+      var dua = parseInt($("#harga_jual").val().replace(/\./g,''));
+      var tiga = parseInt($("#harga_variabel").val().replace(/\./g,''));
+      var empat = parseInt($("#fix_cost").val().replace(/\./g,''));
+      var lima = parseInt($("#company_target").val().replace(/\./g,''));
+
+
+      var cmtotal = (satu*dua)-(satu*tiga);
+      var cmunit = (dua-tiga);
+      var cmpersen = (cmunit/dua)*100;
+      var bep1 = empat/(dua-tiga);
+      var tpunit = (empat+lima)/cmunit;
+      var tpsales = (empat+lima)/(cmpersen/100);
+      var cfcunit = (empat+lima)/cmunit;
+
+      var bep = $("#operator").val();
+      if(bep == "bep"){
+        // $("#hasil").text(empat/(dua-tiga));
+        document.getElementById("hasil").innerHTML = "<br>  Hasil BEP adalah : " + bep1 ;
+
+      } else if (bep == "cma"){
+        document.getElementById("hasil").innerHTML =
+        "<br> Hasil total margin kontribusi Adalah : " + cmtotal +
+        "<br> Hasil unit margin kontribusi Adalah : " + cmunit + " Unit" +
+        "<br> Hasil persen margin kontribusi Adalah : " + cmpersen + "%";
+        // $("#hasil").text(satu-dua);
+      } else if (bep == "tp"){
+        document.getElementById("hasil").innerHTML =
+        "<br> Hasil unit Total Provit Adalah : " + tpunit + " Unit" +
+        "<br> Hasil sales Total Provit Adalah : " + tpsales ;
+        // $("#hasil").text(satu-dua);
+      } else if (bep == "cfc"){
+        document.getElementById("hasil").innerHTML =
+        "<br> Hasil Changed in Fixed Cost Adalah : " + cfcunit ;
+        // $("#hasil").text(satu-dua);
+      }
+    });
 });
+
+function reset()
+{
+  $("#total_unit").val('');
+  $("#harga_jual").val('');
+  $("#harga_variabel").val('');
+  $("#fix_cost").val('');
+  $("#company_target").val('');
+}
--- simulation.php
+++ simulation.php
@@ -1,40 +1,40 @@
 <div class="row">
   <div class="col-md-6">
-    <form action="" method="">
+    <form id="form_cvp" method="POST">
       <div class="form-group" id="total_unit_group">
         <label class="col-sm-8 col-form-label"><b>Total Unit yang terjual per bulan</b></label>
         <div class="col-sm-12">
-          <input type="number" class="form-control"  id="total_unit" placeholder="Total Unit Per Bulan">
+          <input type="text" class="form-control" name = 'satu' id="total_unit" placeholder="Total Unit Per Bulan">
         </div>
       </div>
       <div class="form-group" id="harga_jual_group">
         <label class="col-sm-8 col-form-label"><b>Harga Jual Per Unit</b></label>
         <div class="col-sm-12">
-          <input type="number" class="form-control" id="harga_jual" placeholder="Harga Jual Per Unit">
+          <input type="text" class="form-control" name = 'dua' id="harga_jual" placeholder="Harga Jual Per Unit">
         </div>
       </div>
       <div class="form-group" id="harga_variabel_group">
         <label class="col-sm-8 col-form-label"><b>Harga Variabel Unit</b></label>
         <div class="col-sm-12">
-          <input type="number" class="form-control" id="harga_variabel" placeholder="Harga Variabel Unit">
+          <input type="text" class="form-control" name ='tiga' id="harga_variabel" placeholder="Harga Variabel Unit">
         </div>
       </div>
       <div class="form-group" id="fix_cost_group">
         <label class="col-sm-8 col-form-label"><b>Fix Cost Per Bulan</b></label>
         <div class="col-sm-12">
-          <input type="number" class="form-control" id="fix_cost" placeholder="Fix Cost Per Bulan">
+          <input type="text" class="form-control" name ='empat' id="fix_cost" placeholder="Fix Cost Per Bulan">
         </div>
       </div>
       <div class="form-group" id="company_target_group">
         <label class="col-sm-8 col-form-label"><b>Company Target</b></label>
         <div class="col-sm-12">
-          <input type="number" class="form-control" id="company_target" placeholder="Company Target">
+          <input type="text" class="form-control" name ='lima' id="company_target" placeholder="Company Target">
         </div>
       </div>
       <div class="form-group">
         <label class="col-sm-8 col-form-label"><b>Pilih Perhitungan</b></label>
         <div class="col-sm-12">
-          <select class="custom-select" name="select_rumus" id="state">
+          <select class="custom-select"  name="operator" id="operator">
             <option selected value="bep">Break Event Point</option>
             <option value="cma">Contribution Margin Approach</option>
             <option value="tp">Total Provit</option>
@@ -44,15 +44,37 @@
       </div>
       <div class="form-group float-right">
         <div class="col-sm-12">
-           <button type="submit" class="btn btn-warning">Reset</button>
+           <button type="reset" class="btn btn-warning" onclick="reset()">Reset</button>
+           <!-- <input type="submit" name = 'simpan' class="btn btn-primary" Value="Hitung"> -->
+
            <button type="submit" class="btn btn-primary">Hitung</button>
+           <!-- <input type="submit" name = 'simpan' class="btn btn-primary" Value="Hitung"> -->
+
         </div>
       </div>
-    </form>
+    <!-- </form> -->
   </div>
   <!-- Result -->
   <div class="col-md-6">
-    <label><b>Result : </b></label>
+    <label><b>Result :  </b></label>
+    <span id='hasil' ></span>
   </div>
   <!-- End Result -->
 </div>
+
+<!-- <script>
+  function sumButton()
+  {
+    let satu = parseInt($("#satu").val());
+    let dua = parseInt($("#dua").val());
+    let bep = $("operator").val();
+
+    if(bep == "bep"){
+      $("#hasil").text(satu+dua);
+    } else if (bep == "cma"){
+      $("#hasil").text(satu-dua);
+    }
+
+
+  }
+</script> -->
